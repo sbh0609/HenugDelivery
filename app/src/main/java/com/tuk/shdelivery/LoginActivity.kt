d235e31506b1511c1dab72a2877a35d1e8394100
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-05-19 12:14:31 +0900

    MainActivity -> LoginActivity 이름 변경

--- app/src/main/java/com/tuk/shdelivery/LoginActivity.kt
+++ app/src/main/java/com/tuk/shdelivery/LoginActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import com.tuk.shdelivery.databinding.ActivityMainBinding // import your view binding class
 
 
-class MainActivity : AppCompatActivity() {
-    // Instantiate view binding class
+class LoginActivity : AppCompatActivity() {
+    // 바인딩 객체 생성
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Initialize view binding
+        // 바인딩
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
