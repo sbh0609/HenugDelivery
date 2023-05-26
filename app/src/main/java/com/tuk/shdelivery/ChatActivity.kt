5b4ea2b568eb259ea18092c52a161ee63d9130bb
sbh0914 <sbh0636@naver.com>
2023-05-26 13:36:24 +0900

    plus chat

--- app/src/main/java/com/tuk/shdelivery/ChatActivity.kt
+++ app/src/main/java/com/tuk/shdelivery/ChatActivity.kt
@@ -2,10 +2,52 @@ package com.tuk.shdelivery
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.*
+import com.tuk.shdelivery.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var database: DatabaseReference
+    private lateinit var adapter: ChatAdapter
+    private var messages = mutableListOf<Message>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat) // replace this with your chat activity layout
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        database = FirebaseDatabase.getInstance().reference
+        binding.rvMessages.layoutManager = LinearLayoutManager(this)
+        adapter = ChatAdapter(messages)
+        binding.rvMessages.adapter = adapter
+
+        val postListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                val message = dataSnapshot.getValue(Message::class.java)
+                if (message != null) {
+                    messages.add(message)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) { /* ... */ }
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) { /* ... */ }
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) { /* ... */ }
+            override fun onCancelled(databaseError: DatabaseError) { /* ... */ }
+        }
+        database.child("messages").addChildEventListener(postListener)
+
+        binding.btnSend.setOnClickListener {
+            val content = binding.etMessage.text.toString()
+            if (content.isNotBlank()) {
+                val message = Message(content) // 여기에 필요한 필드를 추가해주세요
+                database.child("messages").push().setValue(message)
+                binding.etMessage.text.clear()
+            }
+        }
     }
 }
+
+data class Message(var content: String="")
+
