f6412bd52b6198de0bcd547e02b41548261233b0
Mint1125 <45926648+Mint1125@users.noreply.github.com>
2023-06-01 15:02:20 +0900

    userDao 설정

--- app/src/main/java/com/tuk/shdelivery/Dao.kt
+++ app/src/main/java/com/tuk/shdelivery/Dao.kt
@@ -15,16 +15,23 @@ class UserDao {
         val db = FirebaseDatabase.getInstance()
         userRef = db.getReference("user")
     }
-
-    //등록
+    /**
+     * input: user <User 객체>
+     * output: void <Void>
+     * 카카오톡 userId,카카오톡 userName 을 작성하고, participateMatchId =0 userPoint =0 으로 들어간다
+     **/
     fun addUser(user: User?): Task<Void>? {
         return userRef?.child(user?.userId.toString())?.setValue(user)
     }
-    //조회
+    /**
+     * input: userId <String>
+     * output: User <User 객체>
+     * userId를 넣으면 User객체를 리턴한다
+     **/
     suspend fun getUser(userId: String): User? = withContext(Dispatchers.IO) {
         val deferred = CompletableDeferred<User?>()
 
-        userRef?.child("users")?.child(userId)?.addListenerForSingleValueEvent(object : ValueEventListener {
+        userRef?.child(userId)?.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 if (dataSnapshot.exists()) {
                     val user = dataSnapshot.getValue(User::class.java)
@@ -41,11 +48,19 @@ class UserDao {
 
         deferred.await()
     }
-    //삭제
-    fun delUser(user: User?): Task<Void>? {
-        return userRef?.child(user!!.userId.toString())?.removeValue()
+    /**
+     * input: userId <String>
+     * output: Void <Void>
+     * userId를 넣으면 해당 user를 삭제한다
+     **/
+    fun delUser(userId: String?): Task<Void>? {
+        return userRef?.child(userId.toString())?.removeValue()
     }
-    //수정
+    /**
+     * input: User <User 객체>
+     * output: Void <Void>
+     * User를 넣으면 해당 userId를 갖은 사용자의 정보를 수정한다
+     **/
     fun updateUser(user: User?): Task<Void>? {
         return userRef?.child(user!!.userId.toString())?.setValue(user)
     }
--- app/src/main/java/com/tuk/shdelivery/MainActivity.kt
+++ app/src/main/java/com/tuk/shdelivery/MainActivity.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.*
 
-
 class MainActivity : AppCompatActivity(), CoroutineScope  {
     // Instantiate view binding class
     private lateinit var binding: ActivityMainBinding
@@ -28,25 +27,31 @@ class MainActivity : AppCompatActivity(), CoroutineScope  {
         job = Job()
         val dao = UserDao()
 
-        binding.btn1.setOnClickListener{
-            var userId: String = "4"
-            var userName: String = "d"
-            var participateMatchId: Long =0
-            var userPoint: Long = 40
 
-            var ex = User(userId, userName, participateMatchId, userPoint)
+        /**
+         * add, get, del, update에 대한 예시
+         * 매개변수는 자체적으로 수정할 수 있다
+         * get을 사용하기 위햐서 coroutine 세팅을 해야한다
+         **/
+
+
+        binding.btn1.setOnClickListener{
+            var userId: String = "6"
+            var userName: String = "f"
+
+            var ex = User(userId, userName, 0, 0)
 
             dao.addUser(ex)
         }
         binding.btn2.setOnClickListener {
             launch {
-                val userId = "1"
+                val userId = "4"
                 val result: User? = dao.getUser(userId)
 
                 if (result != null) {
-                    val userAge = result.userName
-                    if (!userAge.isNullOrEmpty()) {
-                        binding.text1.text = userAge
+                    val userPoint = result.userPoint
+                    if (!userId.isNullOrEmpty()) {
+                        binding.text1.text = userPoint.toString()
                     } else {
                         binding.text1.text = "User age is null or empty"
                     }
@@ -54,6 +59,22 @@ class MainActivity : AppCompatActivity(), CoroutineScope  {
                     binding.text1.text = "User not found"
                 }
             }
+        }
+        binding.btn3.setOnClickListener{
+            var userId: String = "4"
+
+            dao.delUser(userId)
+        }
+
+        binding.btn4.setOnClickListener{
+            var userId: String = "4"
+            var userName: String = "d"
+            var participateMatchId: Long =0
+            var userPoint: Long = 50
+
+            var ex = User(userId, userName, participateMatchId, userPoint)
+
+            dao.updateUser(ex)
         }
     }
 }
