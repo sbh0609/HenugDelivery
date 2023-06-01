a09cca19262473e05c2877b91a7147d67fa01f0c
alsruch02 <64555392+alsruch02@users.noreply.github.com>
2023-06-01 13:50:20 +0900

    Login UI 변경 + 로그인 기능

--- app/src/main/java/com/tuk/shdelivery/Activity/LoginActivity.kt
+++ app/src/main/java/com/tuk/shdelivery/Activity/LoginActivity.kt
@@ -1,8 +1,17 @@
 package com.tuk.shdelivery.Activity
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import com.tuk.shdelivery.databinding.ActivityLoginBinding
 
 
@@ -12,16 +21,67 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // 카톡에서 네이티브키 복사 후 붙혀넣기
+        KakaoSdk.init(this, "네이티브키")
         bd = ActivityLoginBinding.inflate(layoutInflater)
 
+        //Log.d(TAG, "keyhash : ${Utility.getKeyHash(this)}")
+        // 이걸로 Logcat 가서 해시 코드 카톡에 올려주세요
         setContentView(bd.root)
 
         bd.login.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+            // 카카오톡 설치 확인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                // 카카오톡 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    // 로그인 실패 부분
+                    if (error != null) {
+                        Log.e(TAG, "로그인 실패 $error")
+                        // 사용자가 취소
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 다른 오류
+                        else {
+                            UserApiClient.instance.loginWithKakaoAccount(
+                                this,
+                                callback = mCallback
+                            ) // 카카오 이메일 로그인
+                        }
+                    }
+                    // 로그인 성공 부분
+                    else if (token != null) {
+                        Log.e(TAG, "로그인 성공 ${token.accessToken}")
+                        nextMainActivity()
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(
+                    this,
+                    callback = mCallback
+                ) // 카카오 이메일 로그인
+            }
             finish()
         }
-        bd.logout.setOnClickListener {
-            finish()
+        bd.aboutLogin.setOnClickListener {
+            var t1 = Toast.makeText(this, "현재 카카오 로그인만 지원됩니다.", Toast.LENGTH_SHORT)
+            t1.show()
         }
+    }
+
+    // 이메일 로그인 콜백
+    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e(TAG, "로그인 실패 $error")
+        } else if (token != null) {
+            Log.e(TAG, "로그인 성공 ${token.accessToken}")
+            nextMainActivity()
+        }
+    }
+
+    private fun nextMainActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
