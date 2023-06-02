159b7dbba0adeb04fd8dab1f015bad6774f40998
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-06-02 13:49:51 +0900

    Update LoginActivity.kt

--- app/src/main/java/com/tuk/shdelivery/Activity/LoginActivity.kt
+++ app/src/main/java/com/tuk/shdelivery/Activity/LoginActivity.kt
@@ -42,104 +42,121 @@ class LoginActivity : AppCompatActivity(), CoroutineScope {
         setContentView(bd.root)
 
         bd.login.setOnClickListener {
-            login()
+            launch {
+                var result = async { Udao.getUser(user.id.toString()) }.await()
+                //새로운 유저라면
+                if (result == null) {
+                    val newUser = User(
+                        userId = user.id.toString(),
+                        userName = user.kakaoAccount?.profile?.nickname!!,
+                    )
+                    intent.putExtra("user", newUser)
+                    async { Udao.addUser(newUser) }.await()
+                } else {
+                    //이미 있는 유저라면 intent에 넣기
+                    intent.putExtra("user", result)
+                }
+                startActivity(intent)
+                finish()
+            }
         }
 
         bd.aboutLogin.setOnClickListener {
             var t1 = Toast.makeText(this, "현재 카카오 로그인만 지원됩니다.", Toast.LENGTH_SHORT)
             t1.show()
         }
-
-
-        //로그인 기록있음
-        if (AuthApiClient.instance.hasToken()) {
-            UserApiClient.instance.accessTokenInfo { _, error ->
-                if (error != null) {
-                    if (error is KakaoSdkError && error.isInvalidTokenError() == true) {
-                        //로그인 필요
-                        login()
-                    } else {
-                        //기타 에러
-                    }
-                } else {
-                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
-                    fetch_UserData()
-                }
-            }
-        }
     }
 
-    private fun login() {
-        // 카카오톡 설치 확인
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-            // 카카오톡 로그인
-            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                // 로그인 실패 부분
-                if (error != null) {
-                    Log.e(TAG, "로그인 실패 $error")
-                    // 사용자가 취소
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-                    // 다른 오류
-                    else {
-                        UserApiClient.instance.loginWithKakaoAccount(
-                            this,
-                            callback = mCallback
-                        ) // 카카오 이메일 로그인
-                    }
-                }
-                // 로그인 성공 부분
-                else if (token != null) {
-                    Log.e(TAG, "로그인 성공 ${token.accessToken}")
-                    fetch_UserData()
-                }
-            }
-        } else {
-            UserApiClient.instance.loginWithKakaoAccount(
-                this,
-                callback = mCallback
-            ) // 카카오 이메일 로그인
-        }
-    }
 
-    private fun fetch_UserData() {
-        UserApiClient.instance.me { user, error ->
-            if (error != null) {
-                Log.e(TAG, "사용자 정보 요청 실패 $error")
-            } else if (user != null) {
-                Log.e(TAG, "사용자 정보 요청 성공 : $user")
-                val intent = Intent(this, HomeActivity::class.java)
-
-                launch {
-                    var result = async { Udao.getUser(user.id.toString()) }.await()
-
-                    //새로운 유저라면
-                    if (result == null) {
-                        val newUser = User(
-                            userId = user.id.toString(),
-                            userName = user.kakaoAccount?.profile?.nickname!!,
-                        )
-                        intent.putExtra("user", newUser)
-                        async { Udao.addUser(newUser) }.await()
-                    } else {
-                        //이미 있는 유저라면 intent에 넣기
-                        intent.putExtra("user", result)
-                    }
-                    startActivity(intent)
-                    finish()
-                }
-            }
-        }
-    }
-
-    // 이메일 로그인 콜백
-    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (error != null) {
-            Log.e(TAG, "로그인 실패 $error")
-        } else if (token != null) {
-            Log.e(TAG, "로그인 성공 ${token.accessToken}")
-            fetch_UserData()
-        }
-    }
+//        //로그인 기록있음
+//        if (AuthApiClient.instance.hasToken()) {
+//            UserApiClient.instance.accessTokenInfo { _, error ->
+//                if (error != null) {
+//                    if (error is KakaoSdkError && error.isInvalidTokenError() == true) {
+//                        //로그인 필요
+//                        login()
+//                    } else {
+//                        //기타 에러
+//                    }
+//                } else {
+//                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+//                    fetch_UserData()
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun login() {
+//        // 카카오톡 설치 확인
+//        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+//            // 카카오톡 로그인
+//            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+//                // 로그인 실패 부분
+//                if (error != null) {
+//                    Log.e(TAG, "로그인 실패 $error")
+//                    // 사용자가 취소
+//                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+//                        return@loginWithKakaoTalk
+//                    }
+//                    // 다른 오류
+//                    else {
+//                        UserApiClient.instance.loginWithKakaoAccount(
+//                            this,
+//                            callback = mCallback
+//                        ) // 카카오 이메일 로그인
+//                    }
+//                }
+//                // 로그인 성공 부분
+//                else if (token != null) {
+//                    Log.e(TAG, "로그인 성공 ${token.accessToken}")
+//                    fetch_UserData()
+//                }
+//            }
+//        } else {
+//            UserApiClient.instance.loginWithKakaoAccount(
+//                this,
+//                callback = mCallback
+//            ) // 카카오 이메일 로그인
+//        }
+//    }
+//
+//    private fun fetch_UserData() {
+//        UserApiClient.instance.me { user, error ->
+//            if (error != null) {
+//                Log.e(TAG, "사용자 정보 요청 실패 $error")
+//            } else if (user != null) {
+//                Log.e(TAG, "사용자 정보 요청 성공 : $user")
+//                val intent = Intent(this, HomeActivity::class.java)
+//
+//                launch {
+//                    var result = async { Udao.getUser(user.id.toString()) }.await()
+//
+//                    //새로운 유저라면
+//                    if (result == null) {
+//                        val newUser = User(
+//                            userId = user.id.toString(),
+//                            userName = user.kakaoAccount?.profile?.nickname!!,
+//                        )
+//                        intent.putExtra("user", newUser)
+//                        async { Udao.addUser(newUser) }.await()
+//                    } else {
+//                        //이미 있는 유저라면 intent에 넣기
+//                        intent.putExtra("user", result)
+//                    }
+//                    startActivity(intent)
+//                    finish()
+//                }
+//            }
+//        }
+//    }
+//
+//    // 이메일 로그인 콜백
+//    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+//        if (error != null) {
+//            Log.e(TAG, "로그인 실패 $error")
+//        } else if (token != null) {
+//            Log.e(TAG, "로그인 성공 ${token.accessToken}")
+//            fetch_UserData()
+//        }
+//    }
 }
